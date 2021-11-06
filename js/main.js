b1e3aca69233c5bcdf3ce94fca40f4363ffdce52
Nathalia2821 <93615552+Nathalia2821@users.noreply.github.com>
2021-11-06 18:04:53 -0500

    Js inicio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function mostrarRegistro(){
+   document.getElementById('registro').style.display = 'block'; 
+   document.getElementById('inicio').style.display = 'none'; 
+}
+
+function mostrarInicio(){
+  document.getElementById('registro').style.display = 'none'; 
+  document.getElementById('inicio').style.display = 'block'; 
+}
+
