c55f8bebd306aeb643e1d2ef95b29ba6f9454d97
Lorena-Legui <aleguiz32805@universidadean.edu.co>
2021-11-06 09:37:20 -0500

    .

--- js/main_pag4.js
+++ js/main_pag4.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.header').height($(window).height());
-    
-   })
 
-   $(".navbar a").click(function(){
+})
+
+$(".navbar a").click(function() {
     $("body,html").animate({
-     scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
-    
-   })
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000)
+
+})
+
+function mostrar() {
+    div = document.getElementById('prendas_azules');
+    div.style.display = '';
+}
